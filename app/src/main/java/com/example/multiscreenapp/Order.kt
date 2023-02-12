0c007cc72165252e6b44aac4545e844fda5c48f0
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-02-12 22:13:07 +0530

    Multi Screen App

---
+++ app/src/main/java/com/example/multiscreenapp/Order.kt
@@ -0,0 +1,16 @@
+package com.example.multiscreenapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Order : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+
+        val tVOrder = findViewById<TextView>(R.id.tVorder)
+        val ordersOfCustomers = intent.getStringExtra(MainActivity.KEY)
+        tVOrder.text = "Order Placed: " + ordersOfCustomers.toString()
+    }
+}
